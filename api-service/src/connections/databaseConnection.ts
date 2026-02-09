f0b1d516b0cbeb9f1dc95cb4cd68ecaef763a150
Mahesh Kumar Gangula <MaheshKumar.Gangula@cognizant.com>
2026-02-09 16:45:59 +0530

    feat: change the variables for creds.

--- api-service/src/connections/databaseConnection.ts
+++ api-service/src/connections/databaseConnection.ts
@@ -2,15 +2,20 @@ import { Sequelize } from "sequelize";
 import { connectionConfig } from "../configs/ConnectionsConfig"
 
 const { database, host, port } = connectionConfig.postgres
-const credentials = connectionConfig.postgres.credentials.split("::")
 
 export const sequelize = new Sequelize({
-    database, password: credentials[1], username: credentials[0], dialect: "postgres", host, port: +port, pool: {
+    database,
+    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME,
+    dialect: "postgres",
+    host,
+    port: +port,
+    pool: {
         max: 2,
         min: 1,
         acquire: 30000,
         idle: 10000
-      }
+    }
 })
 
  export const health = async () => {
